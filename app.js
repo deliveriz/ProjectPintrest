9d01d672d5abf29384ad5a3654f10da8335640d0
inga <ingvildelise@outlook.com>
2024-05-06 17:34:36 +0200

    auth0

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { auth } = require('express-openid-connect');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true
+};
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
 
 var indexRouter = require('./routes/index');
 var picturesRouter = require('./routes/pictures');
@@ -16,6 +26,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(auth(config));
+
 app.use(fileUpload());
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,6 +37,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'pictures')));
 app.use('/', indexRouter);
 app.use('/pictures', picturesRouter);
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
